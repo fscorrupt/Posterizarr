61e8c50cbe5bda0d40d522243d5fe1a07fb7f0f6
bullmoose20 <nick.zampieri@hotmail.com>
2024-03-01 21:09:56 -0500

    Update PlexPosterMaker.ps1 to see if PosterChoices.csv

--- PlexPosterMaker.ps1
+++ PlexPosterMaker.ps1
@@ -2025,13 +2025,15 @@ else {
     $FormattedTimespawn = $hours.ToString() + "h " + $minutes.ToString() + "m " + $seconds.ToString() + "s "
 
     Write-log -Message "Finished, Total posters created: $posterCount | Total Season Posters created: $SeasonCount" -Path $global:ScriptRoot\Logs\Scriptlog.log -Type Success
-    Write-log -Message "You can find a detailed Summary of Poster Choices here: $global:ScriptRoot\Logs\PosterChoices.csv" -Path $global:ScriptRoot\Logs\Scriptlog.log -Type Info
-    # Calculate Summary
-    $SummaryCount = Import-Csv -LiteralPath "$global:ScriptRoot\Logs\PosterChoices.csv" -Delimiter ';'
-    $FallbackCount = @($SummaryCount | Where-Object Fallback -eq 'True')
-    $TextlessCount = @($SummaryCount | Where-Object Textless -eq 'True')
-    $TextTruncatedCount = @($SummaryCount | Where-Object TextTruncated -eq 'True')
-    $TextCount = @($SummaryCount | Where-Object Textless -eq 'False')
+    if ((Test-Path "$global:ScriptRoot\Logs\PosterChoices.csv")) {
+        Write-log -Message "You can find a detailed Summary of Poster Choices here: $global:ScriptRoot\Logs\PosterChoices.csv" -Path $global:ScriptRoot\Logs\Scriptlog.log -Type Info
+        # Calculate Summary
+        $SummaryCount = Import-Csv -LiteralPath "$global:ScriptRoot\Logs\PosterChoices.csv" -Delimiter ';'
+        $FallbackCount = @($SummaryCount | Where-Object Fallback -eq 'True')
+        $TextlessCount = @($SummaryCount | Where-Object Textless -eq 'True')
+        $TextTruncatedCount = @($SummaryCount | Where-Object TextTruncated -eq 'True')
+        $TextCount = @($SummaryCount | Where-Object Textless -eq 'False')    
+    }
 
     if ($TextlessCount) {
         Write-log -Subtext "'$($TextlessCount.count)' times the script took a Textless poster" -Path $global:ScriptRoot\Logs\Scriptlog.log -Type Warning
