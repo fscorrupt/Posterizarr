258057cc478e0d848bd3e13fe4cf39a64d0fd772
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2025-07-13 18:43:27 +0200

    Update Posterizarr.ps1

--- Posterizarr.ps1
+++ Posterizarr.ps1
@@ -15,7 +15,7 @@ param (
 )
 Set-PSReadLineOption -HistorySaveStyle SaveNothing
 
-$CurrentScriptVersion = "1.9.70"
+$CurrentScriptVersion = "1.9.69"
 $global:HeaderWritten = $false
 $ProgressPreference = 'SilentlyContinue'
 $env:PSMODULE_ANALYSIS_CACHE_PATH = $null
