7995d458aa34e6521f6687b545f6f654fa387587
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2025-10-27 18:51:57 +0100

    code cleanup

--- webui/backend/core/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Core modules for Posterizarr Backend"""
-from .config import *
-from .cache import cache, cached, get_cache_stats, clear_cache, invalidate_cache_pattern
--- webui/backend/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Services package for Posterizarr Backend
-Contains business logic and background services
-"""
-from .asset_service import AssetService, get_asset_service
-
-__all__ = ["AssetService", "get_asset_service"]
